b7f1ad21749294a252d3df63243329df1747cadd
Aalia-Siddique <159708417+Aalia-Siddique@users.noreply.github.com>
2024-09-17 01:44:15 -0700

    Add files via upload

---
+++ Tic Toe Game/script.js
@@ -0,0 +1,75 @@
+let boxes=document.querySelectorAll(".box");
+let restBtn=document.querySelector("#reset-btn");
+let newBtn=document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+
+let turnO=true;
+const winPatterns=
+[[0,1,2],
+[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],
+[6,7,8]];
+boxes.forEach((box) => {
+    box.addEventListener("click",()=>{
+    if(turnO)//plater o turn
+    {
+        box.innerText='O';
+        turnO=false
+    }
+    else{//player x turn
+        box.innerText='X';
+        turnO=true;
+    }
+    box.disabled=true;
+    checkWinner();
+    });
+});
+const disabledBoxes=()=>
+    {
+        for(b of boxes)
+        {
+            b.disabled=true;
+            
+        }
+};
+const showWiner=(winner)=>{
+    msg.innerText=`Congratulations , Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
+};
+const checkWinner=() =>
+{
+    for(let pattern of winPatterns)
+    {
+        let pos1Val=boxes[pattern[0]].innerText;
+        let pos2Val=boxes[pattern[1]].innerText;
+        let pos3Val=boxes[pattern[2]].innerText;
+        if(pos1Val!=""&&pos2Val!=""&&pos3Val!="")
+        {
+            if(pos1Val==pos2Val&&pos2Val==pos3Val)
+            {
+                console.log("winner is:",pos1Val);
+                showWiner(pos1Val);
+            }
+        }
+
+    }
+};
+const enabledBoxes=()=>
+    {
+        for(b of boxes)
+        {
+            b.disabled=false;
+            b.innerText="";
+            
+        }
+};
+const resetGame=()=>{
+    turnO=true;
+    enabledBoxes();
+    msgContainer.classList.add("hide");;
+
+};
+newBtn.addEventListener("click",resetGame);
+restBtn.addEventListener("click",resetGame);
+
